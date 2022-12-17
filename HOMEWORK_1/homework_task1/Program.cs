9265f9586f7eed95b3469dca528174118cafcc3f
Dony <dony.kg1@gmail.com>
2022-12-17 23:15:58 +0600

    Добавлено домашнее задание № 2, вывод максимального числа из трех введенных

---
+++ HOMEWORK_1/homework_task1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*  Напишите программу, которая принимает на вход три числа
+ и выдаёт максимальное из этих чисел.*/
+
+using static System.Console;
+
+Write("Введите первое число: ");
+
+int a = int.Parse(ReadLine()!);
+
+Write("Введите второе число: ");
+
+int b = int.Parse(ReadLine()!);
+
+Write("Введите третье число: ");
+
+int c = int.Parse(ReadLine()!);
+
+int max = a;
+ 
+if (max < b)
+
+max = b;
+
+if (max < c)
+
+max = c;
+
+Write($"Максимальное число {max}");
