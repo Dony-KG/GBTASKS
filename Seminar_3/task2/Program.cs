5e1f3f1e06e4c5e7e943c4a4f515b89547c6489b
Dony <dony.kg1@gmail.com>
2022-12-25 17:41:46 +0600

    добавлена задача, которая определяет дни недели в команде switch

---
+++ Seminar_3/task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+
+Write("Введите цифру дня недели: ");
+
+int day = int.Parse(ReadLine()!);
+
+switch (day)
+{
+    case 1: 
+    {
+        WriteLine("Это понедельник");
+        break;
+    }
+
+    case 2:
+    {
+        WriteLine("Это вторник");
+        break;
+    }
+
+    case 3:
+    {
+        WriteLine("Это среда");
+        break;
+    }
+
+    case 4: 
+    {
+        WriteLine("Это четверг");
+        break;
+    }
+
+    case 5: 
+    {
+        WriteLine("Это пятница");
+        break;
+    }
+
+    case 6:
+    {
+        WriteLine("Это суббота");
+        break;
+    }
+
+    case 7:
+    {
+        WriteLine("Это воскресенье");
+        break;
+    }
+    default:
+    {
+        WriteLine("Вы ввели неправильную цифру");
+        break;
+    }
+}
