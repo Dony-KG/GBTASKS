da99437b8cd117a4c989ead4a7b04bb7efc54e0a
Dony-KG <120251354+Dony-KG@users.noreply.github.com>
2022-12-25 16:55:25 +0600

    Delete task8 directory

--- task8/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using static System.Console;
-Clear();
-Write("Введите первое число: ");
-int a = int.Parse(ReadLine()!);
-Write("Введите второе число: ");
-int b = int.Parse(ReadLine()!);
-
-WriteLine(a==b?"=":a>b?">":"<");
