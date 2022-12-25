4f02e5d3d979fdb503e3692447b16c0e4143f803
Dony-KG <120251354+Dony-KG@users.noreply.github.com>
2022-12-25 16:54:25 +0600

    Delete task10 directory

--- task10/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using static System.Console;
-Clear();
-Write("Введите первое число: ");
-int a = int.Parse(ReadLine()!);
-Write("Введите второе число: ");
-int b = int.Parse(ReadLine()!);
-
-WriteLine(a/b==b?$"{a} является квадратом {b}":b/a==a?$"{b} является квадратом {a}":"числа не являются квадратом другого");
