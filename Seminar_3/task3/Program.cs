2b336ad7bf44fade59a1628734f19c6aa147ac88
Dony <dony.kg1@gmail.com>
2022-12-25 17:44:18 +0600

    добавлена задача, которая определяет какая четверть, по заданному Х и Y

---
+++ Seminar_3/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Определение четверти на координатах
+
+using static System.Console;
+Clear();
+Write("Введите X: ");
+int x = int.Parse(ReadLine()!);
+Write("Введите Y: ");
+int y = int.Parse(ReadLine()!);
+
+
+if (x>0 && y>0)
+{
+    WriteLine("Первая четверть");
+}
+if (x<0 && y>0)
+{
+    WriteLine("Вторая четверть");
+}
+if (x<0 && y<0)
+{
+    WriteLine("Третья четверть");
+}
+if (x>0 && y<0)
+{
+    WriteLine("Четвертая четверть");
+}
