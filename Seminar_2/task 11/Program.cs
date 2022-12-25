656b345ef82502fea97ed1d05d16c98f68566fbb
Dony <dony.kg1@gmail.com>
2022-12-25 16:04:42 +0600

    добавлена задача, которая считает сколько банок было всего

---
+++ Seminar_2/task 11/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+Clear();
+Write("Введите количество банок первого стрелка: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите количество банок второго стрелка: ");
+int b = int.Parse(ReadLine()!);
+int total = a + b - 1;
+WriteLine("банок было " + total);
