2d1898d0d8abb59d3e11d956d32b131d5b129af9
Dony <dony.kg1@gmail.com>
2022-12-25 16:08:09 +0600

    добавлена задача, которая удаляет из трехзначного числа цифру по середине

---
+++ Seminar_2/task6/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число
+ и удаляет вторую цифру этого числа.*/
+
+using static System.Console;
+Clear();
+int number=new Random().Next(100,999);
+WriteLine(number);
+int first=number/100;
+int last=number%10;
+
+Write($"{first}{last}");
