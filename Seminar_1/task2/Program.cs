88a40d0b611b1a6207e25b512930724003098c9e
Dony <dony.kg1@gmail.com>
2022-12-25 15:57:25 +0600

    Добавлена задача, ввод цифр и определения дней недели по этим цифрам

---
+++ Seminar_1/task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+
+Write("Введите цифру дня недели: ");
+
+int day = int.Parse(ReadLine()!);
+
+if (day < 7 && day > 1)
+{
+    if (day == 1) 
+    {
+        WriteLine("Это понедельник");
+    }
+
+        if (day == 2) 
+        {
+        WriteLine("Это вторник");
+        }
+
+            if (day == 3) 
+            {
+            WriteLine("Это среда");
+            }
+
+                if (day == 4) 
+                {
+                WriteLine("Это четверг");
+                }
+
+                    if (day == 5) 
+                    {
+                    WriteLine("Это пятница");
+                    }
+
+                        if (day == 6) 
+                        {
+                        WriteLine("Это суббота");
+                        }
+
+                            if (day == 6) 
+                            {
+                            WriteLine("Это воскресенье");
+                            }
+}
+else
+{
+WriteLine("Вы ввели неправильную цифру");
+}
