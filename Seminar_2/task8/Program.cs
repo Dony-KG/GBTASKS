42d8ad3dcbfa09092d21bfd82e676a79a65cbef8
Dony <dony.kg1@gmail.com>
2022-12-25 16:11:08 +0600

    добавлена задача вывод тернанрными операторами , сравнение двух чисел… равно, больше или меньше

---
+++ Seminar_2/task8/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+Clear();
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+WriteLine(a==b?"=":a>b?">":"<");
