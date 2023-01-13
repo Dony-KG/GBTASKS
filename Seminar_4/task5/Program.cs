b0f0e9eb5221ea5a39eeac8fdf5bb1290f80d45f
Dony <dony.kg1@gmail.com>
2023-01-13 21:58:46 +0600

    Добавлена задача, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке

---
+++ Seminar_4/task5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+using static System.Console;
+Clear();
+int[] array= InitBinaryArray();
+PrintArray(array);
+
+
+
+int[] InitBinaryArray()
+{
+    int[] res = new int[8];
+    for (int i=0; i<res.Length; i++)
+    res[i]= new Random().Next(2);
+    return res;
+}
+
+
+
+void PrintArray(int[] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    Write($"{arr[i]} ");
+}
