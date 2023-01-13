104bdaa8952520b54d9f2458b36d9d94b3f8af28
Dony <dony.kg1@gmail.com>
2023-01-13 22:45:44 +0600

    Добавлена задача, которая выводит массив из указаных  элементов в коде, заполненный нулями и единицами в случайном порядке

---
+++ Seminar_4/task6/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+//Clear();
+
+int [] arr = GetArray(10);
+
+int [] arr1 = new int[12];
+PrintArray(arr);
+WriteLine();
+FullArray (arr1);
+PrintArray(arr1);
+
+void FullArray (int [] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(0, 2);
+}
+}
+
+int[] GetArray (int size)
+{
+int [] array = new int [size];
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(2);
+}
+return array;
+}
+
+void PrintArray (int [] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Write($"{array[i]} ");
+}
+}
