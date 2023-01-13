02bd9e10a17e3e52fccacbbe0291454f8f592aa7
Dony <dony.kg1@gmail.com>
2023-01-13 20:49:13 +0600

    Добавлена задача, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А

---
+++ Seminar_4/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+using static System.Console;
+Clear();
+Write("Введите число : ");
+int.TryParse(ReadLine(),out int number1);
+int sum1 = GetSum(number1);
+WriteLine($"Сумма чисел от 1 до {number1} = {sum1}");
+
+Write("И еще раз введите число : ");
+int.TryParse(ReadLine(),out int number2);
+int sum2 = GetSum(number2);
+WriteLine($"Сумма чисел от 1 до {number2} = {sum2}");
+
+
+int GetSum(int A)
+{
+int result = 0;
+
+while(A > 0)
+{
+result += A;
+A--;
+}
+return result;
+}
