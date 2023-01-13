c43e0f28291c7f1c2d8639039064b0de84739a4a
Dony <dony.kg1@gmail.com>
2023-01-13 23:00:40 +0600

    Добавлена задача, которая выводит массив из указаных  элементов, заполненным в диапозоне указанном пользователем случайном порядке

---
+++ Seminar_4/task7/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using static System.Console;
+//Clear();
+WriteLine("Введите длинну массива");
+int.TryParse(ReadLine(),out int size);
+WriteLine("Введите диапозон чисел (включительно) от: ");
+int.TryParse(ReadLine(),out int num1);
+WriteLine("до: ");
+int.TryParse(ReadLine(),out int num2);
+
+
+int [] arr = GetArray(size, num1, num2);
+
+PrintArray(arr);
+
+
+int[] GetArray (int Size, int Num1, int Num2)
+{
+int [] array = new int [Size];
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(Num1, Num2+1);
+}
+return array;
+}
+
+void PrintArray (int [] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Write($"{array[i]} ");
+}
+}
