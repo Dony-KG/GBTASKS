56a6a0fbe441885592f1715a1b09dadb8f7ece06
Dony <dony.kg1@gmail.com>
2023-01-13 20:58:55 +0600

    Добавлена задача, которая принимает на вход число и выдаёт количество цифр в числе

---
+++ Seminar_4/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//8 -> 2
+//89126 -> 5
+
+using static System.Console;
+Clear();
+Write("Введите число : ");
+int.TryParse(ReadLine(),out int num);
+int count = GetCount(num);
+WriteLine($"Число {num} состоит из  {count} чисел");
+
+
+int GetCount(int A)
+{
+int result = 0;
+
+while(A > 0)
+{
+ A /= 10;   
+result ++;
+}
+return result;
+}
