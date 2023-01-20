41b4b400572475c08b328b1c87f6b6a5fb1b11a4
Dony <dony.kg1@gmail.com>
2023-01-20 23:41:09 +0600

    Добавлена задача №4, которая которая определяет, присутствует ли заданное число в массиве.

---
+++ Seminar_5/task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int.TryParse(ReadLine(), out int number);
+
+int[] array = {-2, -1, -4, 5, 6, 7, 0};
+
+WriteLine(ArrayLookUp(array, number)? "да" : "нет");
+
+bool ArrayLookUp(int[] inArray, int num)
+{
+    foreach (int i in inArray)
+    {
+        if(num == i) return true;
+    }
+    return false;
+}
