d1765a6b899b255eb3fa50035e9da0cc23d2af75
Dony <dony.kg1@gmail.com>
2023-01-20 22:49:23 +0600

    Добавлена задача №2, которая меняет знак элементов массива

---
+++ Seminar_5/task2/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+using static System.Console;
+Clear();
+
+Write("Введите размер массива, мин и макс через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int[] array = GetArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]));
+
+PrintArray(array);
+WriteLine();
+ReverseArray(array);
+PrintArray(array);
+
+
+int [] GetArray( int size, int minValue, int maxValue){
+    int[] resultArray = new int [size];
+
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = random.Next(minValue, maxValue +1);
+    }
+
+    return resultArray;
+}
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        array[i] *= -1;
+    }
+
+}
+
+void PrintArray(int[] array)
+{
+    Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Write($"{array[i]}, ");
+    }
+
+    WriteLine($"{array[array.Length - 1]}]");
+}
