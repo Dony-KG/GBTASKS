5607a1e8ea3cd426f94d04de17f0182028e2cb7c
Dony <dony.kg1@gmail.com>
2023-01-20 22:04:36 +0600

    Корректировка задания №1

--- Seminar_5/task1/Program.cs
+++ Seminar_5/task1/Program.cs
@@ -5,9 +5,9 @@
 */
 
 using static System.Console;
-Clear();
+//Clear();
 Write("Введите размер массива, мин и макс через пробел: ");
-string[] parameters = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
 int[] array = GetArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]));
 printArray(array);
