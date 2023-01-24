99376fc6040b4451524f7b6cd355588e44d47337
Dony <dony.kg1@gmail.com>
2023-01-24 17:43:25 +0600

    Добавлена задача №1, которая принимает на вход три числа и проверяет может ли существовать треугольник с сторонами такой длины.

---
+++ Seminar_6/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая принимает на вход три числа и проверяет,
+//  может ли существовать треугольник с сторонами такой длины.
+
+using static System.Console;
+Clear();
+Write("Введите три стороны треугольника через пробел: ");
+string[] sides = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+WriteLine(Triangle(sides) ? "Треугольник с такими сторонами существует":"Треугольник с такими сторонами не существует");
+
+bool Triangle(string[] side)
+{
+if (int.Parse(side[0]) + int.Parse(side[1])>int.Parse(side[2]) &&
+    int.Parse(side[1]) + int.Parse(side[2])>int.Parse(side[0]) &&
+    int.Parse(side[0]) + int.Parse(side[2])>int.Parse(side[1])) return true;
+
+return false;
+}
