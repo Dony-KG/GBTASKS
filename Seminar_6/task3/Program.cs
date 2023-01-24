a4d3963269f86549ccc190cf65a26eae8f2b0a28
Dony <dony.kg1@gmail.com>
2023-01-24 18:25:29 +0600

    Добавлена задача №3, которая будет преобразовывать десятичное число в двоичное.

---
+++ Seminar_6/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+Write($"Число {number} в двоичной системе будет: {DecimalToBinary(number)}");
+
+
+string DecimalToBinary(int numb)
+{
+    if (numb == 0) return "0";
+    string result = string.Empty;
+    int rem;
+    while (numb > 0)
+    {
+        rem = numb % 2;
+        numb /= 2;
+        result = rem.ToString() + result;
+    }
+    return result;
+}
