00a7ed242875a72c6229ad6e23e3cc2179deaf3d
Dony <dony.kg1@gmail.com>
2023-01-27 02:26:25 +0600

    Добавлена задача № 2, 8 семинара, которая меняет в двумерном массиве заменяет строки на столбцы

--- Seminar_8/task2/Program.cs
+++ Seminar_8/task2/Program.cs
@@ -1,1 +1,68 @@
-﻿
+﻿//  Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы. В случае, если это невозможно, 
+// программа должна вывести сообщение для пользователя.
+
+using static System.Console;
+Clear();
+WriteLine("Введите размер матрицы и диапозон значений через пробел: ");
+string[] parametrs = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+int[,] array = GetMatrixArray(int.Parse(parametrs[0]), int.Parse(parametrs[1]), int.Parse(parametrs[2]), int.Parse(parametrs[3]));
+
+PrintMatrixArray(array);
+WriteLine();
+NewMatrix(array);
+
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] resultArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],3} ");
+        }
+        WriteLine();
+    }
+}
+void NewMatrix(int[,] inArray)
+{
+    int[,] result = new int[inArray.GetLength(0), inArray.GetLength(1)];
+
+    if (inArray.GetLength(0) != inArray.GetLength(1))
+    {
+
+        WriteLine("Невозможно");
+
+    }
+
+    else
+    {
+
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < inArray.GetLength(1); j++)
+            {
+                result[j, i] = inArray[i, j];
+            }
+        }
+        PrintMatrixArray(result);
+
+
+    }
+}
