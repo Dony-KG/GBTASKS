5d2a514a0a1bb03ac676a8fa0b36278919d6fa7e
Dony <dony.kg1@gmail.com>
2023-01-27 02:14:03 +0600

    Добавлена задача № 3, 8 семинара, которая заменяет в двумерном массиве меняет строки на столбцы

---
+++ Seminar_8/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы. В случае, если это невозможно, 
+// программа должна вывести сообщение для пользователя.
+void NewMatrix (int[,] inArray)
+{   
+    int [,] result = new int [inArray.GetLength(0), inArray.GetLength(1)];
+    
+    if (inArray.GetLength(0) != inArray.GetLength(1))
+    {
+        
+        WriteLine ("Невозможно");
+       
+    } 
+    
+    else 
+    {
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+           result[j, i] = inArray [i, j]; 
+        }
+    }
+    PrintMatrixArray(result);
+    
+
+    }
+}
