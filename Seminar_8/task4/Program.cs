8815537da61792075e837766003fe20c0067a801
Dony <dony.kg1@gmail.com>
2023-01-29 00:29:10 +0600

    Корректировка задачи № 4, 8 семинара, теперь программа удаляет все строки и столцы где есть минимальное значение элементов

--- Seminar_8/task4/Program.cs
+++ Seminar_8/task4/Program.cs
@@ -3,68 +3,115 @@
 using static System.Console;
 Clear();
 
-// WriteLine("Введите размер матрицы и диапозон значений через пробел: ");
-// string[] parametrs = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-// int[,] matrix = GetMatrixArray(int.Parse(parametrs[0]), int.Parse(parametrs[1]), int.Parse(parametrs[2]), int.Parse(parametrs[3]));
+WriteLine("Введите размер матрицы и диапозон значений через пробел: ");
 
-int[,] matrix = new int[,] { { 11, 12, 13 }, { 21, 22, 23 }, { 31, 32, 1 } };
+int[] intParams = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+int[,] matrix = GetMatrixArray(intParams[0], intParams[1], intParams[2], intParams[3]);
+
 PrintMatrix(matrix);
-
 WriteLine();
-DeletRowAndColumn(matrix);
 
+DeleteRowsAndColumnsWhithMin(matrix);
 
-void DeletRowAndColumn(int[,] inMatrix)
+void DeleteRowsAndColumnsWhithMin(int[,] inMatrix)
 {
-    int min = inMatrix[0, 0];
-    int k = 0, m = 0;
-    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    int minValue = FindMinValue(inMatrix);
+
+    int[,] result = new int[inMatrix.GetLength(0), inMatrix.GetLength(1)];
+
+    int[] indexRowsAndColumns = FindIndexMinRowsAndColumns(inMatrix);
+
+    for (int k = 0; k < indexRowsAndColumns.Length; k += 2)
     {
-        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        int row = indexRowsAndColumns[k];
+        int column = indexRowsAndColumns[k + 1];
+
+        for (int i = 0; i < inMatrix.GetLength(0); i++)
         {
-            if (inMatrix[i, j] <= min)
+            for (int j = 0; j < inMatrix.GetLength(1); j++)
             {
-                min = inMatrix[i, j];
-                k = i;
-                m = j;
+                result[i, j] = (i == row || j == column) ? 0 : inMatrix[i, j];
+            }
+
+        }
+        for (int i = 0; i < inMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < inMatrix.GetLength(1); j++)
+            {
+                inMatrix[i, j] = result[i, j];
             }
         }
-
     }
-    WriteLine($"Строка: {k}  Столбец: {m}  Минимум последний: {min}");
-
-    int[,] result = new int[inMatrix.GetLength(0) - 1, inMatrix.GetLength(1) - 1];
-
-    for (int i = 0, l = 0; i < inMatrix.GetLength(0); i++, l++)
-    {
-        if (i == k) i++;
-        if (i < inMatrix.GetLength(0))
-            for (int j = 0, n = 0; j < inMatrix.GetLength(1); j++, n++)
-            {
-                if (j == m) j++;
-                if (j < inMatrix.GetLength(1))
-                    result[l, n] = inMatrix[i, j];
-            }
-
-    }
-    PrintMatrix(result);
+    PrintMatrix1(result); WriteLine();
 }
 
 
 
-//     int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
-// {
-//     Random rnd = new Random();
-//     int[,] resultArray = new int[rows, columns];
-//     for (int i = 0; i < rows; i++)
-//     {
-//         for (int j = 0; j < columns; j++)
-//         {
-//             resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
-//         }
-//     }
-//     return resultArray;
-// }
+
+int FindMinValue(int[,] inMatrix)
+{
+    int minValue = inMatrix[0, 0];
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            if (minValue > inMatrix[i, j])
+            {
+                minValue = inMatrix[i, j];
+            }
+        }
+
+    }
+    return minValue;
+}
+
+int CountMinValue(int[,] inMatrix)
+{
+    int inMinValue = FindMinValue(inMatrix);
+    int counter = 0;
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            counter += inMinValue == inMatrix[i, j] ? 1 : 0;
+        }
+    }
+    return counter;
+}
+
+int[] FindIndexMinRowsAndColumns(int[,] inMatrix)
+{
+    int minValue = FindMinValue(inMatrix);
+    int[] result = new int[CountMinValue(inMatrix) * 2];
+    int counter = 0;
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            if (minValue == inMatrix[i, j])
+            {
+                result[counter] = i;
+                result[counter + 1] = j;
+                counter += 2;
+            }
+        }
+    }
+    return result;
+}
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] resultArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return resultArray;
+}
 
 void PrintMatrix(int[,] inMatrix)
 {
@@ -72,8 +119,25 @@ void PrintMatrix(int[,] inMatrix)
     {
         for (int j = 0; j < inMatrix.GetLength(1); j++)
         {
-            Console.Write($"{inMatrix[i, j],4} ");
+            Write($"{inMatrix[i, j],4} ");
         }
-        Console.WriteLine();
+        WriteLine();
+    }
+}
+
+void PrintMatrix1(int[,] inMatrix)
+{
+    int k = 0;
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            if (inMatrix[i, j] != 0)
+            {
+                Write($"{inMatrix[i, j],4} ");
+                k = j;
+            }
+            
+        }if (inMatrix[i, k] != 0) WriteLine();
     }
 }
