c2199a8432612e447f5638b78227adedb929f1f6
Dony <dony.kg1@gmail.com>
2023-01-31 02:46:11 +0600

    Добавлена задача № 3, 9 семинара, которая будет принимать на вход число и возвращать сумму его цифр через рекурсию

---
+++ Seminar_9/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+using static System.Console;
+Clear();
+
+WriteLine(SumNumber(12345));
+
+
+int SumNumber(int num)
+{
+    if (num == 0) return 0;
+    else
+    {
+        return SumNumber(num / 10) + num % 10;
+    }
+}
