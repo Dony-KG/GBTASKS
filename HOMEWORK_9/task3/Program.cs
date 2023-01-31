17026123c0fbc5672823f9784d7c1b24c415db6a
Dony <dony.kg1@gmail.com>
2023-01-31 13:50:42 +0600

    Добавлена ДЗ № 3, 9 семинара, которая реализующую метод вычисления функции Аккермана с помощью рекурсии.

---
+++ HOMEWORK_9/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу реализующую метод вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using static System.Console;
+Clear();
+
+WriteLine(Akk(2, 3));
+WriteLine(Akk(4, 0));
+WriteLine(Akk(3, 0));
+WriteLine(Akk(0, 10));
+WriteLine(Akk(0, 2));
+WriteLine(Akk(1, 2));
+WriteLine(Akk(2, 0));
+
+int Akk(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
