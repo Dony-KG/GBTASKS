9598d5b1d020d46992e331f0aad648ddc6563c42
Dony <dony.kg1@gmail.com>
2023-01-31 13:49:18 +0600

    Добавлена ДЗ № 2, 9 семинара, которая реализует рекурсивный метод нахождения суммы натуральных элементов в промежутке от M до N.

---
+++ HOMEWORK_9/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая реализует рекурсивный метод 
+//нахождения суммы натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите диапозон целых значений, которые нужно сложить, через пробел: ");
+int[] num = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+WriteLine(SumNumber(num[0], num[1]));
+
+
+int SumNumber(int start, int end)
+{
+    return start < end ? (start == end ? start : SumNumber(start, end - 1) + end)
+                        : (start == end ? start : SumNumber(start, end + 1) + end);
+}
